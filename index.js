2151fc4281194fdd554b9d1e2d8fa87675acdd89
agstnrdz <agstnrodriguez@gmail.com>
2025-04-28 14:53:42 -0300

    update-js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+// Lista de imágenes para el header
+const headerImages = [
+  "https://pbs.twimg.com/media/Ggmha1VWEAAGQbH?format=jpg&name=large",
+  "https://agstnrdz.github.io/img/Maserati_1958.jpg",
+  "https://agstnrdz.github.io/img/Argentina_Chubut_topo.png"
+];
+// -----
+// Selecciono el elemento img del header
+const headerImg = document.querySelector(".header-image img");
+
+let currentImageIndex = 0;
+
+function changeHeaderImage() {
+  // Incrementa el índice y vuelve a 0 si llega al final
+  currentImageIndex = (currentImageIndex + 1) % headerImages.length;
+
+  // Aplica efecto de fundido (fade)
+  headerImg.style.opacity = 0;
+
+  // Luego de medio segundo cambia la imagen y vuelve a subir la opacidad
+  setTimeout(() => {
+    headerImg.src = headerImages[currentImageIndex];
+    headerImg.style.opacity = 1;
+  }, 500);
+}
+
+// Cambia la imagen cada 10 segundos (10000 milisegundos)
+setInterval(changeHeaderImage, 10000);
 
