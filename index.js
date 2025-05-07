c483b647a6dac986d477d89b42972d199a1ad1eb
agstnrdz <agstnrodriguez@gmail.com>
2025-05-07 08:55:31 -0300

    update-js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Lista de imágenes para el header
+// ---------- Lista de imágenes para el header ---------- //
 const headerImages = [
   "https://pbs.twimg.com/media/Ggmha1VWEAAGQbH?format=jpg&name=large",
   "https://agstnrdz.github.io/img/Maserati_1958_header.png",
@@ -15,28 +15,25 @@ let currentImageIndex = 0;
 function changeHeaderImage() {
   // Incrementa el índice y vuelve a 0 si llega al final
   currentImageIndex = (currentImageIndex + 1) % headerImages.length;
-  // -----
   // Aplica efecto de fundido (fade)
   headerImg.style.opacity = 0;
-  // -----
   // Luego de medio segundo cambia la imagen y vuelve a subir la opacidad
   setTimeout(() => {
     headerImg.src = headerImages[currentImageIndex];
     headerImg.style.opacity = 1;
   }, 500);
 }
-// -----
 // Cambia la imagen cada 10 segundos (10000 milisegundos)
 setInterval(changeHeaderImage, 10000);
 // -----
-// Animación de entrada suave para las tarjetas
+// ---------- Animación de entrada suave para las tarjetas ---------- //
 document.addEventListener("DOMContentLoaded", () => {
     const cards = document.querySelectorAll('.card, .service-card, presentacion-container, presentacion, presentacion-destacada');
 
     cards.forEach((card, index) => {
         setTimeout(() => {
             card.classList.add('visible');
-        }, index * 200); // escalonado cada 200ms
+        }, index * 200); // Escalonado cada 200ms
     });
 });
 // -----
